a51b7b797dc62d1d00a1f075782cd0d55f46fb95
Ron Caspi <ronroncaspi@gmail.com>
2024-08-16 09:37:02 +0300

    Made functional login

---
+++ src/components/UserButton.svelte
@@ -0,0 +1,15 @@
+<script>
+    import PocketBase from 'pocketbase'
+    import { onMount } from 'svelte';
+
+    let pb
+    onMount(() => {
+        pb = new PocketBase(import.meta.env.PUBLIC_POCKETBASE)
+    })
+</script>
+
+{#if pb?.authStore?.model}
+    <a href="/settings" class="btn btn-secondary">{pb.authStore.model.username}</a>
+{:else}
+    <a href="/login" class="btn btn-primary text-white">Log in</a>
+{/if}
